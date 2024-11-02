168a633050299db32e3ff5073be9d3e8e872ce1b
songhaechan <bukak2019@naver.com>
2024-11-02 22:08:51 +0900

    spring init

---
+++ src/test/java/com/study/blockingqueue/BlockingqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.blockingqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
